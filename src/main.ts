ecbca5a395ed6e7ee35a5da4d236cc4c70296270
Babajide Ojo <babajide.ojo@konga.com>
2024-04-27 15:20:16 +0100

    chore

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const corsOptions: CorsOptions = {
-  origin: ['http://localhost:3000', 'https://nrv-backend-dev.onrender.com'], // Allow requests from this origin
+  origin: ['http://localhost:3000', 'https://nrv-frontend.vercel.app'], // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specified HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specified headers
   credentials: true, // Allow sending cookies and credentials
