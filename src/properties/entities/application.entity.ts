8d1ed94a57506995bc96ac6edff027efed7c2fff
Babajide Ojo <babajide.ojo@konga.com>
2024-09-17 10:59:03 +0100

    chore

--- src/properties/entities/application.entity.ts
+++ src/properties/entities/application.entity.ts
@@ -27,55 +27,14 @@ export class Application {
   currentEmployer: string;
 
   @Prop()
-  jobTitle: string;
+  reasonForLiving: string;
 
   @Prop()
   monthlyIncome: number;
 
   @Prop()
-  jobStartDate: Date;
+  currentResidence: string;
 
-  @Prop()
-  currentLandlord: string;
-
-  @Prop()
-  currentAddress: string;
-
-  @Prop()
-  reasonForLeaving: string;
-
-  @Prop()
-  leaseStartDate: Date;
-
-  @Prop()
-  leaseEndDate: Date;
-
-  @Prop({ default: false })
-  criminalRecord: boolean;
-
-  @Prop()
-  criminalRecordDetails: string;
-
-  @Prop()
-  referenceName: string;
-
-  @Prop()
-  referenceNumber: string;
-
-  @Prop()
-  numberOfVehicles: number;
-
-  @Prop()
-  petNumber: number;
-
-  @Prop()
-  smoker: boolean;
-
-  @Prop()
-  evictionHistory: boolean;
-
-  @Prop()
-  evictionDetails: string;
 }
 
 
