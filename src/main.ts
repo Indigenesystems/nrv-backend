ed1703586f26c62bb9bd6340f3f99a958fea148e
Babajide Ojo <ojobabajide629@gmail.com>
2026-05-01 14:07:01 +0100

    chore

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ const corsOptions: CorsOptions = {
     'https://nrv-frontend-main.vercel.app',
     'https://www.naijarentverify.com',
     'https://nrv-admin-fe.vercel.app',
-    'https://admin-hub-fv4h.onrender.com'
+    'https://admin-hub-fv4h.onrender.com',
+    'https://nrv-frontend-zjim.onrender.com'
   ], // Allow requests from these origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
